9f637415204b9faf19aeffddfbae64b6cff608c1
jesperdejonge <jesperdejonge@users.noreply.github.com>
2021-10-20 10:25:39 +0200

    Create Label.py

---
+++ cryptoMLtrading/featureGen/Label.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[4]:
+
+
+#Assign label to df
+#Label 1 = Reached high without reaching low
+#Label 2 = Reached neither high or low
+#Label 3 = Reached high and low
+#Label 4 = Reached low without reaching high
+
+def LABEL(dataframe):
+    for index,row in dataframe.iterrows():
+        increase = (dataframe["high"][index] / dataframe["open"][index]) - 1
+        decrease = (1 - (dataframe["low"][index]/dataframe["open"][index]))
+        if increase > 0.01 and decrease < 0.005:
+            dataframe.at[index-1,"label"] = 1
+        elif increase < 0.005 and decrease > 0.01:
+            dataframe.at[index-1,"label"] = 4
+        elif increase > 0.01 and decrease > 0.01:
+            dataframe.at[index-1,"label"] = 3
+        else:
+            dataframe.at[index-1,"label"] = 2
+
+    return dataframe
+
+
+# In[ ]:
+
+
+
+
